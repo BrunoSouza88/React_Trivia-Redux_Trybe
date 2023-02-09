26a3abe4d584f47e56e26ef6978879e367001ed3
Igor <igoraraujobda@gmail.com>
2023-02-09 15:59:24 -0300

    requisito 1 - criação da rota de login, os inputs e as validações

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import logo from './trivia.png';
 import './App.css';
+import Login from './pages/Login';
 
-export default function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>SUA VEZ</p>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={ logo } className="App-logo" alt="logo" />
+          <p>SUA VEZ</p>
+        </header>
+        <div>
+          <Switch>
+            <Route exact path="/" component={ Login } />
+          </Switch>
+        </div>
+      </div>
+    );
+  }
 }
+
+export default App;
