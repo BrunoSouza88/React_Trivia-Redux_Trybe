a96a825b986a9dc919182faeba5cc2b96f6e66c5
eversondias <eversondias@pop-os.localdomain>
2023-02-10 15:29:25 -0300

    [grupo 19] requisito 14

--- src/redux/actions/actionTypes.js
+++ src/redux/actions/actionTypes.js
@@ -1,3 +1,5 @@
 export const ADD_NAME = 'ADD_NAME';
-
 export const ADD_EMAIL = 'ADD_EMAIL';
+export const ADD_SCORE = 'ADD_SCORE';
+export const ADD_ASSERTIONS = 'ADD_ASSERTIONS';
+export const START_GAME = 'START_GAME';
