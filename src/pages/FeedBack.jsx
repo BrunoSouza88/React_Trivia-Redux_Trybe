4ace0e4aabd176ce450671a45cc7e1233f3b9a6f
Igor <igoraraujobda@gmail.com>
2023-02-10 15:24:37 -0300

    requisito 13 - criação da tela de feedback e mensagem de feedback

--- src/pages/FeedBack.jsx
+++ src/pages/FeedBack.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
 
+INICIAL_STATE = {
+  feebackMsg: '',
+};
 class FeedBack extends React.Component {
+  componentDidMount() {
+    this.generateScoreFeeback();
+  }
+
+  generateScoreFeeback = () => {
+    // const { score } = this.props;
+    const score = 4;
+    const scoreLine = 3;
+    if (score < scoreLine) {
+      this.setState({ feebackMsg: 'Could be better...' });
+    }
+    this.setState({ feebackMsg: 'Well Done!' });
+  };
+
   render() {
+    const { feebackMsg } = this.state;
     return (
-      <p>
-        FeedBack
+      <p
+        data-testid="feedback-text"
+      >
+        { feebackMsg }
       </p>
+
     );
   }
 }
