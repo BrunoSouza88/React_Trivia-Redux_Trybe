16e87dc66714f2df5710e8f42bc9c3324db73654
BrunoSouza <desouza88@hotmail.com>
2023-02-10 15:52:37 -0300

    Requisito 19 component Ranking feito

---
+++ src/components/Ranking.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+class Ranking extends React.Component {
+  render() {
+    const { player } = this.props;
+
+    savingRankingLocalStorage = () => {
+      localStorage.setItem('Ranking', JSON.stringify({
+        name: player.name,
+        score: player.score,
+        picture: urlDaFotoGravatar,
+      }));
+
+      // savingTokenLocalStorage = () => {
+      //   localStorage.setItem('token', tokenRecebidoPelaAPI);
+      // };
+    };
+
+    return (
+      <div>
+        <h1>Component Ranking</h1>
+        <img src="ImagemVindaDoGravatar" alt="imagem do avatar do jogador" />
+        <p data-testid={ `player-name-${player.name}` } />
+        <p data-testid={ `player-score-${player.score}` } />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (globalState) => ({
+  player: globalState.player,
+});
+
+Ranking.propTypes = {
+  player: PropTypes.shape({
+    name: PropTypes.string,
+    score: PropTypes.number,
+  }).isRequired,
+};
+
+export default connect(mapStateToProps)(Ranking);
