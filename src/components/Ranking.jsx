b010aaeb3aaa9826a18829d80e43a22bc26aff37
Igor <igoraraujobda@gmail.com>
2023-02-13 18:32:10 -0300

    requisito 20 em andamento

--- src/components/Ranking.jsx
+++ src/components/Ranking.jsx
@@ -5,27 +5,6 @@ import { Link } from 'react-router-dom';
 import md5 from 'crypto-js/md5';
 
 class Ranking extends React.Component {
-  // componentDidMount() {
-  //   this.savingRankingLocalStorage();
-  // }
-
-  // getPlayers = () => {
-  //   const allPlayers = localStorage.getItem('Ranking');
-  //   return allPlayers ? JSON.parse(allPlayers) : [];
-  // };
-
-  //  savingRankingLocalStorage = () => {
-  //   const { player } = this.props;
-
-  //   const playerObj = {
-  //     name: player.name,
-  //     score: player.score,
-  //     picture: player.gravatarEmail,
-  //   };
-  //   const allPlayersRanking = this.getPlayers();
-  //   localStorage.setItem('Ranking', JSON.stringify([...allPlayersRanking, playerObj]));
-  // };
-
   render() {
     const allPlayers = JSON.parse(localStorage.getItem('Ranking'));
     return (
@@ -33,7 +12,7 @@ class Ranking extends React.Component {
         <div>
           <h1 data-testid="ranking-title">Tela de Ranking</h1>
           {
-            allPlayers.sort((a, b) => b.score - a.score).map((player, index) => ( // allplayers nao esta sendo reconhecido como array
+            allPlayers.sort((a, b) => b.score - a.score).map((player, index) => (
               <div key={ index }>
                 <img src={ `https://www.gravatar.com/avatar/${md5(player.piture).toString()}` } alt="imagem do avatar do jogador" />
                 <p data-testid={ `player-name-${index}` }>{ player.name }</p>
